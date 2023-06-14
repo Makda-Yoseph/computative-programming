1a1f6bc5680c3d39f7a44284dce79c709922168d
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2023-06-14 08:20:02 +0300

    Create string.py
    string add built in function

---
+++ string.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+return_country = set()
+for i in range(n):
+    return_country.add((input()))
+print(len(return_country))
+
+    
